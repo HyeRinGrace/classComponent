2b21344901987388519fc95ffaa478d969b4276e
Hyerin Kim <kdjsanh0217@naver.com>
2024-03-21 22:39:23 +0900

    기본 이미지 설정

--- src/component/box.js
+++ src/component/box.js
@@ -5,10 +5,13 @@ class Box extends Component {
   render() {
     const { title, item, result, color } = this.props;
 
+    // 초기 이미지 URL
+    const initialImgUrl = 'https://cdn-icons-png.flaticon.com/512/5261/5261874.png';
+
     return (
       <div className='boxContainer'>
         <h1>{title}</h1>
-        <img className="scissors" src={item && item.img} alt="scissors"></img>
+        <img className="scissors" src={item ? item.img : initialImgUrl} alt="scissors"></img>
         <div className='winnerContainer' style={{ backgroundColor: color }}>
           <div className="winnerJudge">{result}</div>
         </div>
